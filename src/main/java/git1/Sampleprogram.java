0ff03b734357821d23304e098a8dcc8d13da25d9
shakthi1610 <shakthisivan1@gmail.com>
2022-08-26 12:49:50 +0530

    ggg

---
+++ src/main/java/git1/Sampleprogram.java
@@ -0,0 +1,17 @@
+package git1;
+
+public class Sampleprogram {
+	private void add() {
+		System.out.println("addition");
+
+	}
+	private void sub() {
+		System.out.println("subtractin");
+
+	}
+	private void mul() {
+		System.out.println("multiplication");
+
+	}
+
+}
